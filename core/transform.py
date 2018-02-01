5ed15fd728023927731e44c3f1fe3b7ea94e84f3
ldescamps85 <32356743+ldescamps85@users.noreply.github.com>
2018-02-01 19:56:06 +0100

    refractoring Laia

--- core/transform.py
+++ core/transform.py
@@ -5,6 +5,49 @@ from sklearn.feature_extraction.text import CountVectorizer
 #pip install emoji #install if not already installed
 import emoji 
 import re
+from sklearn.model_selection import train_test_split
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+def obtain_data_representation(df, boW_size=200, test=None):
+    # If there is no test data, split the input
+    if test is None:
+        # Divide data in train and test
+        train, test = train_test_split(df, test_size=0.25)
+        df.airline_sentiment = pd.Categorical(df.airline_sentiment)
+    else:
+        # Otherwise, all is train
+        train = df
+
+    # Create a Bag of Words (BoW), by using train data only
+    vocabular_bi = get_bigram(df.text, boW_size)
+    vocabular_bi.update({'😭': boW_size, '😆': boW_size + 1, '👍': boW_size + 2})
+
+    cv = CountVectorizer(vocabulary=vocabular_bi)
+
+    x_train = cv.fit_transform(train['text'])
+    y_train = train['airline_sentiment'].values
+
+    # Obtain BoW for the test data, using the previously fitted one
+    x_test = cv.transform(test['text'])
+    try:
+        y_test = test['airline_sentiment'].values
+    except:
+        # It might be the submision file, where we don't have target values
+        y_test = None
+
+    return {
+        'train': {
+            'x': x_train,
+            'y': y_train
+        },
+        'test': {
+            'x': x_test,
+            'y': y_test
+        }
+    }
+
+
 
 def uppercase_ratio_extract(text):
     upper = 0
@@ -30,18 +73,16 @@ def tokenizer_extraction(df):
     return df
 
 
-#data = df["text"]
-
 def get_unigram(data):
     count_model = CountVectorizer(ngram_range=(1, 1))
     X = count_model.fit_transform(data)
     count_model.vocabulary_
-
     return X
 
 
 def get_bigram(data,boW_size):
-    bigram_vectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b',stop_words='english', min_df=1,max_features=boW_size)
+    bigram_vectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b',stop_words='english', min_df=1,
+                                        max_features=boW_size,tokenizer=LemmaTokenizer())
     return bigram_vectorizer.vocabulary_
 
 
@@ -65,8 +106,3 @@ def add_emoji_column_to_df(df):
     df['emoji']=df['text'].apply(lambda x: extract_emojis([x]))
     return(df)
 
-#unig = get_unigram(texts)
-#unig.toarray()  # matriz de unigrams
-
-#big = get_bigram(texts)
-#big.toarray()  # matriz --> casi todo 0 porque mucha info
