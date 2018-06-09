578922a25cfa95a994029d05d0790b518f45c112
jsantalo <jsantajor@gmail.com>
2018-06-09 16:54:23 +0200

    kaggle summit file in spanish is coded as UTF-8. added feature in dataset loader to specify the encoding. just be carefull girls!

--- input/load_data.py
+++ input/load_data.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
 
-def load_dataset(filename='', lan='english'):
+def load_dataset(filename='', encoding='', lan='english'):
 
     if lan == 'english':
-        encoding = 'utf-8'
+        if encoding == '':
+            encoding = 'utf-8'
         if filename == '':
             filename = '../happybirds/data/tweets_public.csv'
     elif lan == 'spanish':
-        encoding = 'utf-16'
+        if encoding == '':
+            encoding = 'utf-16'
         if filename == '':
             filename = '../happybirds/data/tweets_public_es.csv'
     else:
